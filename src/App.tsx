122fa2f14ffd33a5cff6b18252128998ac606832
FrancoStoll <francostoll@gmail.com>
2024-01-13 20:04:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,22 @@
+const HeroSection = () => {
+    return (
+        <section className="mt-6 container mx-auto">
+            <div className="md:flex h-screen">
+                <div className="md:w-1/2 border-red-400 border flex justify-center items-center">
+                    Lorem ipsum dolor
+                    <br />
+                    sit amet consectetur
+                    <br />
+                    adipisicing.
+                </div>
+
+                <div className="md:w-1/2">
+                    adios
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
