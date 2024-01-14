0218ce0ac2f863a138074af996626a6fe24d03e6
remotefrank <francostoll2@gmail.com>
2024-01-14 14:51:40 -0300

    herosection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,17 +1,25 @@
 const HeroSection = () => {
     return (
-        <section className="mt-6 container mx-auto">
-            <div className="md:flex h-screen">
-                <div className="md:w-1/2 border-red-400 border flex justify-center items-center">
-                    Lorem ipsum dolor
-                    <br />
-                    sit amet consectetur
-                    <br />
-                    adipisicing.
+        <section className="container mx-auto">
+            <div className="md:flex h-[calc(100vh-4rem)]">
+                <div className="md:w-1/2 flex flex-col justify-center p-5 items-start gap-3">
+                  <h1 className="text-4xl">Mira nuestros <span className="text-blue-900">productos,</span><br />
+                  contactanos para mas <span className="text-blue-900">información.</span>
+                  </h1>
+                  <p className="text-gray-700 md:w-[25rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates suscipit ab inventore cum maxime consequatur, optio sint cumque dignissimos, ratione unde eos.</p>
+
+                  <div className="flex gap-3">
+                    <button className="uppercase px-4 py-3 bg-black text-white rounded shadow hover:bg-blue-900 focus:outline-none focus:shadow-outline transition-all transition-duration:150ms">
+                        Ver Productos
+                    </button>
+                    <button className="uppercase px-4 py-3 shadow-xl text-black rounded  hover:bg-blue-900 focus:outline-none focus:shadow-outline transition-all transition-duration:150ms">
+                        Contactanos
+                    </button>
+                  </div>
                 </div>
 
-                <div className="md:w-1/2">
-                    adios
+                <div className="md:w-2/2">
+                    <img src="./heroimage.jpg" alt="heroimg" className="w-full h-full object-cover filter brightness-75" />
 
                 </div>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,15 @@
 
 const Navbar = () => {
     return (
-        <header>
+        <header className="shadow-lg h-16 sticky">
             <div className="container flex justify-between m-auto rounded">
-                <div className="w-32">
-                    <img src="https://us.123rf.com/450wm/logoking33/logoking332011/logoking33201100066/159766658-plantilla-de-vector-de-dise%C3%B1o-de-logotipo-de-carta-ie-ei-dise%C3%B1o-del-logotipo-ie-ei.jpg?ver=6" alt="logo" />
+                <div className="w-20 cursor-pointer">
+                    <img  src="https://us.123rf.com/450wm/logoking33/logoking332011/logoking33201100066/159766658-plantilla-de-vector-de-dise%C3%B1o-de-logotipo-de-carta-ie-ei-dise%C3%B1o-del-logotipo-ie-ei.jpg?ver=6" alt="logo" />
                 </div>
 
 
                 <nav className="flex items-center">
-                    <ul className="hidden md:flex md:gap-3 md:text-xl">
+                    <ul className="hidden md:flex md:gap-3  text-gray-700">
                         <li className="cursor-pointer">Inicio</li>
                         <li className="cursor-pointer">Productos</li>
                         <li className="cursor-pointer">Contacto</li>
@@ -18,7 +18,7 @@ const Navbar = () => {
                     </ul>
                 </nav>
                 <div className="flex items-center gap-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 cursor-pointer">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                     <div className="md:hidden px-3">
